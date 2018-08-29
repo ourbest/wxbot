11539bb616071f30643bf3a84fbeaf5be7d4e7bb
ourbest <yonghui.chen@cutt.com>
2018-08-29 19:05:25 +0800

    save data

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ IMAGE=registry.cutt.com/p/wxbot
 docker pull $IMAGE
 docker rm -f wxbot
 docker run -d \
-    -v /data/wxbots:/data \
+    -v /data/wxbots:/code/data \
+    -v /data/wxbots/bots:/code/bots \
     -p 5000:5000 \
     --name wxbot \
     $IMAGE
