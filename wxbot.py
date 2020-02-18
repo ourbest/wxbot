243fdbec34a025c521f1137d864c4e712feb2afd
yonghui <yonghui.chen@linkingcities.com>
2020-02-18 23:21:46 +0800

    ignore func

--- wxbot.py
+++ wxbot.py
@@ -48,7 +48,8 @@ def get_bots():
 
 @app.before_request
 def after_request():
-    if request.path in ('/', '/bot/login', '/favicon.ico', '/bot/post', '/check') or request.path.find('/dist') == 0:
+    if request.path in ('/', '/bot/login', '/favicon.ico',
+                        '/bot/post', '/check', '/bot/members') or request.path.find('/dist') == 0:
         pass
     elif not is_login():
         raise Unauthorized()
